895d67aba2f3a1351951af43ac04f8c76876e2b7
Rithanyaa.B <rithanyaa.virtue@gmail.com>
2026-03-03 22:09:53 +0530

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,20 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        UserManagerInterface userManager = new UserManager();
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+          String[] UserArray=new String[5];
+          System.out.println("Enter your choice");
+          System.out.println("1.Create Task");
+          System.out.println("2.Read Task");
+          System.out.println("3.Update Task");
+          System.out.println("4.Delete Task");
+          int choice= new Integer(sc.nextLine());
 
-        while (true) {
-            userManager.user();
-            TaskManagerInterface taskmanager = new TaskManager(userManager.getCurrentUser());
-            Integer choice = 0;
-            while (choice != 5) {
-                try {
+          if(choice==1){
 
-                    System.out.println("What you need?");
-                    System.out.println("1.Add task");
-                    System.out.println("2.List Task");
-                    System.out.println("3.Update");
-                    System.out.println("4.Delete");
-                    System.out.println("5.Exit");
-                    System.out.println("Enter your choice:");
-                    choice = Integer.valueOf(sc.nextLine());
-                    if (choice < 1 || choice > 5) {
-                        throw new Exception();
-                    }
-                } catch (Exception e) {
-                    System.out.println("Enter proper value");
-                    continue;
-                }
-                if (choice == 1) {
-                    taskmanager.addTask();
-                }
-                else if (choice == 2) {
-                    taskmanager.Display();
-                }
-                else if (choice == 3) {
-                    taskmanager.update();
-                }
-                else if (choice == 4) {
-                    taskmanager.delete();
-                }
-                else if (choice == 5) {
-                    System.out.println("Task Manager Exited");
-                    break;
-                }
-                else {
-                    System.out.println("Enter a value choice");
-                }
-            }
+          }
 
-        }
     }
 }
